67c743fc179383c770332eb103b162c1f507fe78
ADIAWIAKOYE <adiawiakoye.le10@gmail.com>
2022-12-17 10:17:41 +0000

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,34 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AcceuilComponent } from './acceuil/acceuil.component';
+import { ConnexionComponent } from './connexion/connexion.component';
+import { InscriptionComponent } from './inscription/inscription.component';
+import { RegionComponent } from './region/region.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path:"",
+    redirectTo:"acceuil",
+    pathMatch:'full'
+  },
+  {
+    path:"inscription",
+    component:InscriptionComponent
+  },
+  {
+    path:"connexion",
+    component:ConnexionComponent
+  },
+  {
+    path:"acceuil",
+    component:AcceuilComponent
+  },
+  {
+    path:"region",
+    component:RegionComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InscriptionComponent } from './inscription/inscription.component';
+import { ConnexionComponent } from "./connexion/connexion.component";
+import { AcceuilComponent } from './acceuil/acceuil.component';
+import { RegionComponent } from './region/region.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        InscriptionComponent,
+        ConnexionComponent,
+        AcceuilComponent,
+        RegionComponent
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+       
+    ]
 })
 export class AppModule { }
